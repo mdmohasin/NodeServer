21368750fcdadab2ff9ddb270ca84759a49bb146
mohd mohasin <mohasin.grad08@gmail.com>
2018-07-18 21:48:45 +0530

    added server name

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -1,3 +1,4 @@
 export const JWT_SECRET = 'NODEBAACKED_key';
 export const DEFAULT_PORT = 8082;
 export const INVALID_PAYLOAD = 'Please provide a valid login payload';
+export const SERVER_NAME="NODE SERVER"
